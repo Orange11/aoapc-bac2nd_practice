0da5aeefa828649127191b8f5efddcddedd41f2a
wangpengcheng <609691039@qq.com>
2015-10-12 23:36:59 +0800

    Add EX 3-1

---
+++ src/Ch3/TEX_Quotes_UVa272.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+   int c,q=1;
+   while((c=getchar())!=EOF){
+    if(c=='"'){
+        printf("%s",q?"``":"''");
+        q=!q;
+    }else{
+    printf("%c",c);
+    }
+   }
+    return 0;
+}
