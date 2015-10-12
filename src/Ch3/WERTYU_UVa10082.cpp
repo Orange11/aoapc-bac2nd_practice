29d1b32f95e051692bc6ce10e990ed7083849129
wangpengcheng <609691039@qq.com>
2015-10-12 23:38:40 +0800

    Add EX 3-2

---
+++ src/Ch3/WERTYU_UVa10082.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+    char s[]="`1234567890-=QWERTYUIOP[]ASDFGHJKL;'\ZXCVBNM,./";
+    int i,c;
+    while((c=getchar())!=EOF){
+        for(i=1;s[i]&&s[i]!=c;i++);
+            if(s[i])
+                putchar(s[i-1]);
+            else
+                putchar(c);
+
+    }
+    return 0;
+}
