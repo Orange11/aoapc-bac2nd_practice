e46c3f00593fe11ddb2c8f687d339910fdaccdc0
wangpengcheng <609691039@qq.com>
2015-10-12 23:41:20 +0800

    Add EX 3-3

---
+++ src/Ch3/Palindromes_UVa401.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cstdio>
+#include<cctype>
+#include<cstring>
+using namespace std;
+
+const char *rev="A   3  HIL JM O   2TUVWXY51SE Z  8 ";
+const char *msg[]={"-- is not a palindrome.","-- is a regular palindrome.","-- is a mirrored string.","-- is a mirrored palindrome."};
+char r(char ch){
+    if(isalpha(ch))
+        return rev[ch-'A'];
+    else
+        return rev[ch-'0'+25];
+
+}
+int main()
+{
+    char s[30];
+    while(scanf("%s",s)==1){
+        int len=strlen(s);
+        int p=1,m=1;
+        for(int i=0;i<(len+1)/2;i++){
+            if(s[i]!=s[len-i-1]) p=0;
+            if(r(s[i])!=s[len-i-1]) m=0;
+        }
+        printf("%s %s\n\n",s,msg[p+2*m]);
+    }
+    return 0;
+}
